86d731b5472c075b0e93ff9c3f44d35f8e3244e0
guoyang328 <yguo@cs.wisc.edu>
2019-08-06 16:39:19 -0500

    ˆ

---
+++ adv.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+def squared_l2_norm(x):
+    flattened = x.view(x.unsqueeze(0).shape[0], -1)
+    return (flattened ** 2).sum(1)
+
+
+def l2_norm(x):
+    return squared_l2_norm(x).sqrt()
+
+
+def adv_loss(model,
+                x_natural,
+                y,
+                optimizer,
+                step_size=0.003,
+                epsilon=0.031,
+                perturb_steps=10,
+                beta=1.0,
+                distance='l_inf'):
+    model.eval()
+    batch_size = len(x_natural)
+    # generate adversarial example
+    x_adv = x_natural.detach() + 0.001 * torch.randn(x_natural.shape).cuda().detach()
+    if distance == 'l_inf':
+        for _ in range(perturb_steps):
+            x_adv.requires_grad_()
+            with torch.enable_grad():
+                loss = nn.CrossEntropyLoss()(model(x_adv), y)
+            grad = torch.autograd.grad(loss, [x_adv])[0]
+            x_adv = x_adv.detach() + step_size * torch.sign(grad.detach())
+            x_adv = torch.min(torch.max(x_adv, x_natural - epsilon), x_natural + epsilon)
+            x_adv = torch.clamp(x_adv, 0.0, 1.0)
+    else:
+        x_adv = torch.clamp(x_adv, 0.0, 1.0)
+    model.train()
+
+    x_adv = Variable(torch.clamp(x_adv, 0.0, 1.0), requires_grad=False)
+    # zero gradient
+    optimizer.zero_grad()
+    # calculate robust loss
+    loss = nn.CrossEntropyLoss()(model(x_adv), y)
+    return loss
+
+
